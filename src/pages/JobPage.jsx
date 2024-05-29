3af0f5917bcebebb9d869f1546a8efdf0fe74309
Hasan Raza <95583135+AhsanJay@users.noreply.github.com>
2024-05-29 07:14:19 +0500

    My First React App

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 const JobPage = () => {
     const { id } = useParams();
-    // const [job, setJob] = useState(null);
-    // const [loading, setLoading] = useState(true)
-    // useEffect(() => {
-    //     const fetchJob = async () => { 
-    //         try {
-    //             const res = await fetch(`/api/jobs/${id}`)
-    //             const data = await res.json();
-    //             setJob(data);
-    //           } catch (error) {
-    //             console.log('Error in Fetching Data', error);
-    //           } finally {
-    //             setLoading(false);
-    //           }
-    //      }
-    //     fetchJob()
-    // }, [])
+    const [job, setJob] = useState(null);
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const fetchJob = async () => { 
+            try {
+                const res = await fetch(`/api/jobs/${id}`)
+                const data = await res.json();
+                setJob(data);
+              } catch (error) {
+                console.log('Error in Fetching Data', error);
+              } finally {
+                setLoading(false);
+              }
+         }
+        fetchJob()
+    }, [])
   return (
     loading ? <Spinner /> : (<h1>{ job.title }</h1> )
 )
